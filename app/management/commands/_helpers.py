97216d2c101e5489bb097991738704439a5d3fa2
Hatim <hatimjuzer78652@gmail.com>
2021-08-20 17:50:33 +0500

    Embedded create dummy user in command

--- app/management/commands/_helpers.py
+++ app/management/commands/_helpers.py
@@ -7,17 +7,21 @@ def check_if_username_exists(username):
 
 
 def create_dummy_user():
-    while True:
-        random_digits = "".join([str(random.randint(0, 9)) for i in range(random.randint(1, 10))])
-        username = "hatim" + random_digits
-        if not check_if_username_exists(username):
-            break
+    try:
+        while True:
+            random_digits = "".join([str(random.randint(0, 9)) for i in range(random.randint(1, 10))])
+            username = "hatim" + random_digits
+            if not check_if_username_exists(username):
+                break
 
-    user_obj = User.objects.create(
-        username=username,
-        first_name='Hatim',
-    )
+        user_obj = User.objects.create(
+            username=username,
+            first_name='Hatim',
+        )
 
-    user_obj.set_password("helloworld123")
-    user_obj.save()
-    return user_obj
+        user_obj.set_password("helloworld123")
+        user_obj.save()
+        return user_obj
+    except Exception as e:
+        print(e)
+        return None
--- app/management/commands/custom_cmd.py
+++ app/management/commands/custom_cmd.py
@@ -1,8 +1,16 @@
 from django.core.management import BaseCommand
 
+from app.management.commands._helpers import create_dummy_user
+
 
 class Command(BaseCommand):
     help = 'Adds dummy users'
 
     def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS("HOOOO!"))
+        self.stdout.write("Creating user...")
+        user_obj = create_dummy_user()
+        self.stdout.write(f"User: {user_obj.username} created successfully.") if user_obj is not None \
+            else self.stdout.write(self.style.ERROR("Could not create user."))
+
+
+
